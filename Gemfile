f7875967389b356d7023317e9459e8564038a967
L. David Varvel <dave@cofunder.net>
2012-06-24 11:51:07 -0700

    Paperclip should now be using S3.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'pg'
 #gem 'sqlite3'
 
 gem 'paperclip'
+gem 'aws-sdk'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/video.rb
+++ app/models/video.rb
@@ -4,6 +4,12 @@ class Video < ActiveRecord::Base
     belongs_to :sender, :class_name => "User"
     belongs_to :recipient, :class_name => "User"
 
-    has_attached_file :video
+    has_attached_file :video,
+      :storage => :s3,
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :s3_credentials => {
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
 
 end
