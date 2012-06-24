94e8d367f8d4e3c456f95fc50a0f093c6d782dde
L. David Varvel <dave@cofunder.net>
2012-06-24 12:20:13 -0700

    Added the 'has_many' relations to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  has_many :sent_videos, :class_name => 'Video', :foreign_key => 'sender_id'
+  has_many :received_videos, :class_name => 'Video', :foreign_key => 'recipient_id'
+
 end
